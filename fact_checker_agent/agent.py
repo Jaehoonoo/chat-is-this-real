89552909cb2f141d9b676bbce9a17577a41bd6aa
kalyanoliveira <kalyan.coliveira@gmail.com>
2025-09-27 10:38:21 -0400

    correclty names the instance of the fact_checker agent

--- fact_checker_agent/agent.py
+++ fact_checker_agent/agent.py
@@ -3,7 +3,7 @@ from .subagents.confidence_score_agent.agent import confidence_score_agent
 from .subagents.evaluator_agent.agent import evaluator_agent 
 from .subagents.retrieval_agent.agent import retrieval_agent
 
-root_agent = Agent(
+fact_checker_agent = Agent(
     model='gemini-2.0-flash-001',
     name='root_agent',
     description='A helpful assistant for user questions.',
