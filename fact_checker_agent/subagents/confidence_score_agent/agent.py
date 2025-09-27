dca4ea2f4ab5c97a383b8232caf0d47d618f3355
kalyanoliveira <kalyan.coliveira@gmail.com>
2025-09-27 10:59:09 -0400

    uses double quotes in attrs of conf score agnt instead of single ones

--- fact_checker_agent/subagents/confidence_score_agent/agent.py
+++ fact_checker_agent/subagents/confidence_score_agent/agent.py
@@ -15,9 +15,9 @@ class ConfidenceScoreOutput(BaseModel):
 CONFIDENCE_SCORE_AGENT_INSTRUCTION = ""
 
 confidence_score_agent = Agent(
-    model='gemini-2.0-flash-001',
-    name='confidence_score_agent',
-    description='A helpful assistant for user questions.',
+    model="gemini-2.0-flash-001",
+    name="confidence_score_agent",
+    description="A helpful assistant for user questions.",
     instruction=CONFIDENCE_SCORE_AGENT_INSTRUCTION,
     output_key="confidence_score"
     output_schema=ConfidenceScoreOutput
