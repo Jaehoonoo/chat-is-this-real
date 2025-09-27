857b666c4d1fbbbfc63f0edfb4e5c7b2b315d6ad
kalyanoliveira <kalyan.coliveira@gmail.com>
2025-09-27 13:26:05 -0400

    abandons the usage of output schema for the confidence score agent

--- fact_checker_agent/subagents/confidence_score_agent/agent.py
+++ fact_checker_agent/subagents/confidence_score_agent/agent.py
@@ -1,18 +1,9 @@
 from google.adk.agents import Agent
 from pydantic import BaseModel
 
-class ConfidenceScoreOutput(BaseModel):
-    confidence_score: float = Field(
-        description = "A number ranging from 0 to 1 indicating the confidence, where 0 is lowest confidence, and 1 is highest confidence"
-    )
-    citations: str = Field(
-        description = ""
-    )
-    explanation: str = Field(
-        description = ""
-    )
+CONFIDENCE_SCORE_AGENT_INSTRUCTION = "
 
-CONFIDENCE_SCORE_AGENT_INSTRUCTION = ""
+"
 
 confidence_score_agent = Agent(
     model="gemini-2.0-flash-001",
@@ -20,5 +11,4 @@ confidence_score_agent = Agent(
     description="A helpful assistant for user questions.",
     instruction=CONFIDENCE_SCORE_AGENT_INSTRUCTION,
     output_key="confidence_score"
-    output_schema=ConfidenceScoreOutput
 )
