7426e136ff2af5c56d19c14fc6fa2bcfc504ba11
kalyanoliveira <kalyan.coliveira@gmail.com>
2025-09-27 19:08:41 -0400

    registers the get source weights tools with the conf score agnt

--- fact_checker_agent/subagents/confidence_score_agent/agent.py
+++ fact_checker_agent/subagents/confidence_score_agent/agent.py
@@ -53,7 +53,8 @@ confidence_score_agent = Agent(
     description="""You are an agent responsible for giving a confidence score
     with regards to how confident you are.""",
     instruction=CONFIDENCE_SCORE_AGENT_INSTRUCTION,
-    output_key="confidence_score"
+    output_key="confidence_score",
+    tools=[get_source_weights]
 )
 
 # Executes required runner logic for unit test of conf score agnt.
