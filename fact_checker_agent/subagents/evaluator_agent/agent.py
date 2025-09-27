283a9053fbbaef184288a6dbe170495852080c6e
kalyanoliveira <kalyan.coliveira@gmail.com>
2025-09-27 10:36:20 -0400

    correctly names the instance of the evaluator agnet

--- fact_checker_agent/subagents/evaluator_agent/agent.py
+++ fact_checker_agent/subagents/evaluator_agent/agent.py
@@ -1,6 +1,6 @@
 from google.adk.agents import Agent
 
-root_agent = Agent(
+evaluator_agent = Agent(
     model='gemini-2.0-flash-001',
     name='root_agent',
     description='A helpful assistant for user questions.',
