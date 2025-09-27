5cca83d346bb21e7dbf7c5832f6fb4c137f9bd86
Joe Brashear <ohrbittheplanet@gmail.com>
2025-09-27 18:43:00 -0400

    add some confidence score things

--- fact_checker_agent/subagents/confidence_score_agent/agent.py
+++ fact_checker_agent/subagents/confidence_score_agent/agent.py
@@ -1,6 +1,16 @@
 from google.adk.agents import Agent
 from pydantic import BaseModel
 
+"""
+{
+    "domain": "example.com",
+    "credibility_score": 0.81,
+    "bias_label": "-0.63",
+    "recency_score": 1.01,
+    "stance": "supports",
+    "reasoning": "The domain is generally reliable, the article aligns with the claim(s), and it was published recently."
+  }
+"""
 class ConfidenceScoreOutput(BaseModel):
     confidence_score: float = Field(
         description = "A number ranging from 0 to 1 indicating the confidence, where 0 is lowest confidence, and 1 is highest confidence"
