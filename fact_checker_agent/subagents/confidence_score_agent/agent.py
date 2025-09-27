f986acfaa4736e3e1032134e6b7cf922657b229b
kalyanoliveira <kalyan.coliveira@gmail.com>
2025-09-27 14:28:40 -0400

    adds missing comma

--- fact_checker_agent/subagents/confidence_score_agent/agent.py
+++ fact_checker_agent/subagents/confidence_score_agent/agent.py
@@ -16,7 +16,7 @@ confidence_score_agent = Agent(
     model="gemini-2.0-flash-001",
     name="confidence_score_agent",
     description="""You are an agent responsible for giving a confidence score
-    with regards to how confident you are."""
+    with regards to how confident you are.""",
     instruction=CONFIDENCE_SCORE_AGENT_INSTRUCTION,
     output_key="confidence_score"
 )
